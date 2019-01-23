abb76fd3a4961c7a45a1fbacfb642cafa949ad6d
Shoumyo <shoumyoarchit@gmail.com>
2019-01-23 00:47:03 -0500

    Consolidated project

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ particlesJS.load('particles', 'resources/particles.json', function() {
     console.log('particles loaded');
 });
 
-fetch("nav2.html")
+fetch("nav.html")
     .then(response => response.text())
     .then(html => {
         document.querySelector("#nav-container").innerHTML = html;
